992a340bebddb9ef8345d5f2b0345a70896db06a
hugo <hugo8072@proton.me>
2025-02-04 16:32:56 +0000

    update frontend
    first steps

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div style={{ backgroundColor: 'black', color: 'white', height: '100vh' }}>
+      <h2>Bem vindo</h2>
+    </div>
+  );
+}
+
+export default Home;
